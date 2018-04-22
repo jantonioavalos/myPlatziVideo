54c39bfff3e482960a6d7b4af45591c11cca60ba
Juan Antonio Avalos <juanantonio.avalosorozco@nxp.com>
2018-04-22 16:10:36 -0500

    Update ProgressBar feature from class 32

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -5,6 +5,7 @@ import Title from '../components/title';
 import PlayPause from '../components/play-pause';
 import Timer from '../components/timer';
 import Controls from '../components/video-player-controls';
+import ProgressBar from '../components/progress-bar';
 
 class VideoPlayer extends Component {
   state = {
@@ -30,6 +31,9 @@ class VideoPlayer extends Component {
       currentTime: this.video.currentTime,
     });
   };
+  handleProgressChange = event => {
+    this.video.currentTime = event.target.value;
+  };
   componentDidMount () {
     this.setState ({
       pause: !this.props.autoPlay,
@@ -44,6 +48,11 @@ class VideoPlayer extends Component {
           <Timer
             currentTime={this.state.currentTime}
             duration={this.state.duration}
+          />
+          <ProgressBar
+            duration={this.state.duration}
+            value={this.state.currentTime}
+            handleProgressChange={this.handleProgressChange}
           />
         </Controls>
         <Video
